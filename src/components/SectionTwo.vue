9d58b256319da716cf496d022f9e59cfb813f5da
Daniel Beltran <danilidonbeltran@gmail.com>
2025-06-13 18:01:13 +0200

    add background images to SectionTwo component; enhance layout with responsive styles

--- src/components/SectionTwo.vue
+++ src/components/SectionTwo.vue
@@ -22,5 +22,54 @@ export default {
 
 .section-2 {
     background-color: #ffffff; /* White */
+    position: relative;
+    overflow: hidden;
+}
+
+.section-2::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 35%;
+    height: 100%;
+    background-image: url('@/assets/bg/purple-square.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: left center;
+    transform: translateY(-50%);
+    z-index: 1;
+}
+
+.section-2::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 20%;
+    width: 35%;
+    height: 100%;
+    background-image: url('@/assets/bg/pink-square.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: left center;
+    transform: translateY(-50%); /* Slightly offset to the bottom */
+    z-index: 2;
+}
+
+@media (max-width: 1280px) {
+    .section-2::before {
+        top: 0;
+        transform: none;
+        width: 100%;
+        height: 50%;
+    }
+    
+    .section-2::after {
+        top: 0;
+        width: 100%;
+        height: 50%;
+        background-position: center;
+        transform: none ;
+    }
 }
 </style>
