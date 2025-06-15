18aa942556d04e194e33ff9e603e6eaee82bc434
Daniel Beltran <danilidonbeltran@gmail.com>
2025-06-15 16:59:21 +0200

    add team section to SectionFour component; include team member details and images

--- src/components/SectionFour.vue
+++ src/components/SectionFour.vue
@@ -1,22 +1,123 @@
 <template>
   <section class="section section-4" id="section-4">
-    <h2>Meet the team</h2>
+    <div class="section-header">
+      <h2>Our Team</h2>
+      <p>Meet the passionate individuals behind LayerZ.</p>
+    </div>
+    <div class="section-content">
+      <div class="grid-container">
+        <div class="grid-item" style="background-color: rgba(113, 44, 246, 0.01)">
+          <img
+            src="@/assets/imgs/mike.jpg"
+            alt="Team Member 1"
+            class="item-image"
+          />
+          <div>
+            <h3>Mike Rogers</h3>
+            <p>CEO and Co-founder</p>
+            <a href="https://www.linkedin.com/in/mike-a-rogers/" aria-label="LinkedIn">
+              <LinkedinIcon class="linkedin-icon" />
+            </a>
+          </div>
+        </div>
+        <div class="grid-item" style="background-color: rgba(243, 56, 158, 0.01)">
+          <img
+            src="@/assets/imgs/marc.jpg"
+            alt="Team Member 2"
+            class="item-image"
+          />
+          <div>
+            <h3>Marc Lamarche</h3>
+            <p>CMO and Co-founder</p>
+            <a href="https://www.linkedin.com/in/marclamarche/" aria-label="LinkedIn">
+              <LinkedinIcon class="linkedin-icon" />
+            </a>
+          </div>
+        </div>
+        <div class="grid-item" style="background-color: rgba(92, 149, 255, 0.01)">
+          <img
+            src="@/assets/imgs/dani.jpg"
+            alt="Team Member 3"
+            class="item-image"
+          />
+          <div>
+            <h3>Daniel Beltran</h3>
+            <p>CTO and Co-founder</p>
+            <a href="https://www.linkedin.com/in/daniellidonbeltran/" aria-label="LinkedIn">
+              <LinkedinIcon class="linkedin-icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
-  <script>
+<script>
+import LinkedinIcon from './icons/LinkedinIcon.vue'
+
 export default {
-  name: 'SectionFour'
-}
+  name: "SectionFour",
+  components: {
+    LinkedinIcon
+  }
+};
 </script>
 
-  <style scoped>
+<style scoped>
 .section {
-    width: 100vw;
-    height: 100vh;
-    position: relative;
-    padding: 60px 80px;
+  width: 100vw;
+  position: relative;
+  padding: 60px 80px;
 }
 .section-4 {
-    background-color: #f7f7f8; 
+  background-color: #FFF;
+}
+
+.section-header {
+  max-width: 1600px;
+  margin-bottom: 40px;
+  color: #270972; /* Indigo */
+}
+
+.section-content {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.grid-container {
+  display: grid;
+  max-width: 1600px;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  gap: 180px;
+  width: 100%;
+}
+.grid-item {
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.item-image {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  border-radius: 8px;
+  margin-bottom: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border: 2px solid #e0e0e0;
+}
+
+.linkedin-icon {
+  width: 24px;
+  height: 24px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: opacity 0.3s ease;
+}
+
+.linkedin-icon:hover {
+  opacity: 0.7;
 }
 </style>
